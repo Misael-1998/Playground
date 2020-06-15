1b4ef1f80ea87f05535421a8f3c288a3a03b80e7
shulkmaster <shulkmaster@gmail.com>
2020-06-15 01:34:15 -0600

    Algoritmo de busqueda lineal

---
+++ Playground/sesion3/LinearSearch.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace Playground.sesion3
+{
+    class LinearSearch
+    {
+
+        public bool Search(int y, params int[] x) {
+            int x2;
+            bool wasFound = false;
+            for (int j = 0; j < x.Length; j++)
+            {
+                x2 = x[j];
+                if (x[j] == y)
+                {
+                    Console.WriteLine($"Se encontro que {y} == {x2}");
+                    wasFound = true;
+                    break;
+                }
+                Console.WriteLine($"Se encontro que {y} != {x2}");
+            }
+            
+            return wasFound;
+        }
+
+        public void PrintUntil(int y, params int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (y == x[i])
+                {
+                    continue;
+                }
+                Console.WriteLine($"Se encontro que {x[i]} xd");
+            }
+        }
+    }
+}
--- Playground/sesion3/ShotGallery.cs
+++ Playground/sesion3/ShotGallery.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Playground.sesion3
 {
     class ShotGallery
@@ -8,24 +10,30 @@ namespace Playground.sesion3
         {
 
             Uzi[] weapons = new Uzi[10];
-            Cartige[] cartiges = new Cartige[20];
+            Cartige[] cartiges = new Cartige[30];
             for (int i = 0; i < weapons.Length; i++)
             {
                 weapons[i] = new Uzi($"usi{i}");
-            }
-            for (int i = 0; i < cartiges.Length; i++)
-            {
-                cartiges[i] = new Cartige(25);
-
+                cartiges[i + 0] = new Cartige(10); // 0--> 9
+                cartiges[i + 10] = new Cartige(4); // 10 --> 19
             }
             for (int x = 0; x < weapons.Length; x++)
             {
-
                 weapons[x].Reaload(cartiges[x]);
-                while (weapons[x].Shoot()) ;
+                while (weapons[x].Shoot());
                 weapons[x].Reaload(cartiges[x + 10]);
-
+                while (weapons[x].Shoot());
             }
+
+            if (new LinearSearch().Search(5, 1,2,3,4,5,7,8,9,1))
+            {
+                Console.WriteLine("Se encoontro el numero");
+            }
+            else {
+                Console.WriteLine("Kyc");
+            }
+
+            new LinearSearch().PrintUntil(5, 1, 2, 5, 4, 5, 7, 8, 5, 1);
         }
     }
 }
