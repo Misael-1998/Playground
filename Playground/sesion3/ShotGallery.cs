d4f1d89fa310f0721ba47a04317b110ecbe59dec
Misael-1998 <misaelgg1998@hotmail.com>
2020-06-16 21:32:57 -0600

    Probando

--- Playground/sesion3/ShotGallery.cs
+++ Playground/sesion3/ShotGallery.cs
@@ -20,20 +20,22 @@ namespace Playground.sesion3
             for (int x = 0; x < weapons.Length; x++)
             {
                 weapons[x].Reaload(cartiges[x]);
-                while (weapons[x].Shoot());
+                while (weapons[x].Shoot()) ;
                 weapons[x].Reaload(cartiges[x + 10]);
-                while (weapons[x].Shoot());
+                while (weapons[x].Shoot()) ;
             }
+            /*
+                        if (new LinearSearch().Search(5, 1,2,3,4,5,7,8,9,1))
+                        {
+                            Console.WriteLine("Se encoontro el numero");
+                        }
+                        else {
+                            Console.WriteLine("Kyc");
+                        }
 
-            if (new LinearSearch().Search(5, 1,2,3,4,5,7,8,9,1))
-            {
-                Console.WriteLine("Se encoontro el numero");
-            }
-            else {
-                Console.WriteLine("Kyc");
-            }
-
-            new LinearSearch().PrintUntil(5, 1, 2, 5, 4, 5, 7, 8, 5, 1);
+                        new LinearSearch().PrintUntil(5, 1, 2, 5, 4, 5, 7, 8, 5, 1);
+                    }
+            */
         }
     }
 }
