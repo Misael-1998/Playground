6e22896e98153bb7fa5765d5663bd15b0a5e0b52
Misael-1998 <misaelgg1998@hotmail.com>
2020-06-17 17:46:43 -0600

    Continuando sesion 7 aun no se a terminado

---
+++ Playground/Sesion7/Fabrica/Frenos/IBrembo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground.Sesion7.Fabrica.Frenos
+{
+    interface IBrembo : IFrenos
+    {
+        public void frenar()
+        {
+            Console.WriteLine("frenar xd");
+        }
+    }
+}
---
+++ Playground/Sesion7/Fabrica/Frenos/IFrenos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground.Sesion7.Fabrica.Frenos
+{
+    interface IFrenos
+    {
+        public void Frenar();
+    }
+}
